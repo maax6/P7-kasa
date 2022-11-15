6099932c0d6c62fb67ff7dd5584c1c21c95618c6
⌘ Maxime <maxmtlfr@gmail.com>
2022-11-15 15:16:49 +0100

    add housing Json and component
    add housing component

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Housing from './pages/Housing'
 import Header from'./components/Header'
 import SignIn from './pages/SignIn'
 import Error from './components/Error'
 import About from './pages/About'
 export default function MyRouter(){
-	return <Router>
-		<Header />
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/SignIn" element={<SignIn />} />
-			<Route path="/About" element={<About />} />
-			<Route path="/Error" element={<Error />} />
-		</Routes>
- </Router>
+	return (<Router>
+			    <Header />
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route path="/SignIn" element={<SignIn />} />
+					<Route path="/Housing/:id" element={<Housing />} />
+					<Route path="/About" element={<About />} />
+					<Route path="/*" element={<Error />} />
+				</Routes>
+		 </Router>
+	 );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,12 @@
 import Banner from '../../components/Banner/'
+import Products from "../../products.json";
 import '../../../src/styles/index.css'
+
 export default function Home() {
    return (
       <div>
          <Banner />
+		 
       </div>
    )
 }
