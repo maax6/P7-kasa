197228c6b88fea1b4fb574c70e5db40d562fee1d
⌘ Maxime <maxmtlfr@gmail.com>
2022-12-28 19:09:14 +0100

    Begin Card components
    Have access to correct ID and object on the single product page

--- src/pages/Card/index.jsx
+++ src/pages/Card/index.jsx
@@ -1,11 +1,19 @@
 import { useParams } from 'react-router-dom'
-export default function Card() {
-   const { id } = useParams()
+import products from '../../assets/templates/products.json';
 
-   return (
-      <div>
-         <h2> Fiche {id} </h2>
-      </div>
-	
-   )
+export default function Card() {
+	const list = JSON.stringify(products)
+	// console.log(list)
+	const {id}  = useParams()
+	const parsedList = JSON.parse(list)
+	const selectedItem = parsedList.find(item => item.id === id) 
+	console.log(selectedItem)
+	// console.log(this.selectedItem.location)
+	console.log(parsedList)
+	// console.log(this.selectedItem.id)
+	return (
+		<div>
+			<h2> Fiche Logement {id} </h2>
+		</div>
+	)
 }
