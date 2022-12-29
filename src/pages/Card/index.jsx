f0484d380cfe84c252341a2c8a13f394e06722a1
⌘ Maxime <85428733+maax6@users.noreply.github.com>
2022-12-29 20:16:59 +0100

    Acces to Products data in card component

--- src/pages/Card/index.jsx
+++ src/pages/Card/index.jsx
@@ -1,19 +1,32 @@
 import { useParams } from 'react-router-dom'
 import products from '../../assets/templates/products.json';
-
+import Error from '../Error'
 export default function Card() {
 	const list = JSON.stringify(products)
-	// console.log(list)
+	//console.log(list)
 	const {id}  = useParams()
 	const parsedList = JSON.parse(list)
 	const selectedItem = parsedList.find(item => item.id === id) 
-	console.log(selectedItem)
-	// console.log(this.selectedItem.location)
+
+	//console.log(selectedItem)
 	console.log(parsedList)
-	// console.log(this.selectedItem.id)
+	if(!selectedItem){
+		return <Error />
+	}
+	console.log(selectedItem.id)
+	console.log(selectedItem.title)
+	console.log(selectedItem.location)
+	console.log(selectedItem.rating)
+	console.log(selectedItem.tags)
+	console.log(selectedItem.host)
+	console.log(selectedItem.equipments)
+	console.log(selectedItem.description)
+	console.log(selectedItem.pictures)
 	return (
-		<div>
-			<h2> Fiche Logement {id} </h2>
-		</div>
+		<div className='card'>
+			<h1>{selectedItem.title} </h1>
+			<ul className='card__tags'>
+			</ul>
+			</div>
 	)
 }
