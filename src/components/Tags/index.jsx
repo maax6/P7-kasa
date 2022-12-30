49c4dcb86c9e5a4d5cdcbe92b18c29c7a5b3ce0f
⌘ Maxime <85428733+maax6@users.noreply.github.com>
2022-12-30 02:52:19 +0100

    add tags component

---
+++ src/components/Tags/index.jsx
@@ -0,0 +1,9 @@
+//function to login on a website
+
+export default function Tags({ getTag }) {
+	return (
+	  <div className="tag">
+		 <p className="tag__text">{getTag}</p>
+	  </div>
+	);
+ }; 
--- src/pages/Card/index.jsx
+++ src/pages/Card/index.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import products from '../../assets/templates/products.json';
 import Error from '../Error'
+import Tags from '../../components/Tags'
 export default function Card() {
 	const list = JSON.stringify(products)
 	//console.log(list)
@@ -13,20 +14,24 @@ export default function Card() {
 	if(!selectedItem){
 		return <Error />
 	}
-	console.log(selectedItem.id)
-	console.log(selectedItem.title)
-	console.log(selectedItem.location)
-	console.log(selectedItem.rating)
-	console.log(selectedItem.tags)
-	console.log(selectedItem.host)
-	console.log(selectedItem.equipments)
-	console.log(selectedItem.description)
-	console.log(selectedItem.pictures)
+	//englobe all tags in a
 	return (
 		<div className='card'>
 			<h1>{selectedItem.title} </h1>
-			<ul className='card__tags'>
-			</ul>
+			<div className='tags'>
+				{selectedItem.tags.map((tag, index) => (
+					<Tags key={index} getTag={tag} />
+					))}
 			</div>
+		</div>
 	)
 }
+															//console.log(selectedItem.id)
+															//console.log(selectedItem.title)
+															//console.log(selectedItem.location)
+															//console.log(selectedItem.rating)
+															//console.log(selectedItem.tags)
+															//console.log(selectedItem.host)
+															//console.log(selectedItem.equipments)
+															//console.log(selectedItem.description)
+															//console.log(selectedItem.pictures)
