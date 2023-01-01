f050545939a8ea31fd557853333aab63260689da
⌘ Maxime <maxmtlfr@gmail.com>
2023-01-01 19:13:35 +0100

    Path indicator onNav

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import logo from '../../assets/logo.svg'
 export default function Header() {
-   return (
-      <header className="header">
+	const pathname = window.location.pathname;
+  	return (
+	<header className="header">
 		<div className="logo__container">
 			<img src={logo} alt="Kasa" className="logo" />
 		</div>
 		<nav className="nav">
-         <ul className="nav__bar">
-            <li className="nav__Link">
-               <a href="/">Acceuil</a>
-            </li>
-            <li className="nav__Link">
-               <a href="/About">À Propos</a>
-            </li>
-         </ul>
-      </nav>
-      </header>
+        	<ul className="nav__bar">
+            	<li className={pathname === '/' ? 'active nav__link' : 'nav__link'} >
+               		<a href="/">Acceuil</a>
+            	</li>
+            	<li className={pathname === '/About' ? 'active nav__link' : 'nav__link'}>
+               		<a href="/About">À Propos</a>
+            	</li>
+         	</ul>
+      	</nav>
+    </header>
    )
 }
